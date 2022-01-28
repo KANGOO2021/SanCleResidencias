be636a4a314b65cda5d1faed991f4924339b2370
Sergio <ser2004munoz@yahoo.com.ar>
2022-01-28 19:24:49 -0300

    SanCleResidencias

--- db/config.js
+++ db/config.js
@@ -4,10 +4,10 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
 
-    user: "gbmdgyqwueaynm",
-    host: "ec2-54-83-137-206.compute-1.amazonaws.com",
-    database: "d6fdj165hdplm6",
-    password: "ec599e18d57e35ef210b7659075d7e95ddc67e256447b70370364901989085b9",
+    user: "vteatzjgbmhrhw",
+    host: "ec2-44-193-111-218.compute-1.amazonaws.com",
+    database: "d685kdc3c7ofuc",
+    password: "2812d7ddcfcfa6eadde641602561b5dcfc0c8a284dcc7b76310ba6123d9c67cf",
     port: 5432,
     ssl: {
         rejectUnauthorized: false
