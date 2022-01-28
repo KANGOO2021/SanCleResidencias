8be168ffc110cb5501221a64445ebf56e30963bf
Sergio <ser2004munoz@yahoo.com.ar>
2022-01-28 18:27:38 -0300

    SanCleResidencias

--- routes/formulario.js
+++ routes/formulario.js
@@ -1,7 +1,13 @@
 const { Router } = require('express');
 const router = Router();
-const { formularioConsultas, formularioConsultasPhone, formulariomensajes, formularioConsultasUpdate, formularioConsultasDelete } = require('../controllers/formulario');
+const {
+    formularioConsultas,
+    formularioConsultasPhone,
+    formulariomensajes,
+    formularioConsultasUpdate,
+    formularioConsultasDelete
 
+} = require('../controllers/formulario');
 
 
 router.get('/consultas', formularioConsultas);
@@ -9,7 +15,6 @@ router.get('/consultas/:telefono', formularioConsultasPhone);
 router.post('/mensajes', formulariomensajes);
 router.put('/consultas/:id', formularioConsultasUpdate);
 router.delete('/consultas/:id', formularioConsultasDelete);
-
 
 
 module.exports = router;
